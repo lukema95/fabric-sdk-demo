dca7336f4e9defae6d62c02d8d87df4179380d28
maluning <32571952+maluning@users.noreply.github.com>
2020-06-05 16:19:50 +0800

    Add files via upload

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -4,13 +4,27 @@ import (
 	"fmt"
 	"github.com/hyperledger/fabric-sdk-go-sample-gm/service"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"os"
 )
 
 var configPath string
 
 func init()  {
+	serviceCmd.PersistentFlags().StringVar(&configPath, "config-path", "./config", "config file path")
+	cobra.OnInitialize(InitConfig)
+	viper.BindPFlag("config-path", serviceCmd.PersistentFlags().Lookup("config-path"))
 	serviceCmd.AddCommand(startCmd)
+}
+
+func InitConfig() {
+	viper.AddConfigPath(configPath)
+	viper.SetConfigType("json")
+	viper.SetConfigName("config")
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
 
 
